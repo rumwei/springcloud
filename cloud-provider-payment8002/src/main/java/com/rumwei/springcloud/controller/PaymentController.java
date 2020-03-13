c3461b3c92fabaca733705212b95ec501903ba91
rumwei <isguwei@163.com>
2020-03-13 17:49:06 +0800

    enableDiscovery

--- cloud-provider-payment8002/src/main/java/com/rumwei/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/rumwei/springcloud/controller/PaymentController.java
@@ -6,7 +6,11 @@ import com.rumwei.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -27,6 +31,21 @@ public class PaymentController {
         Payment result = paymentService.getPaymentById(id);
         if (result != null) return new CommonResult(200,"查询成功,serverPort: "+serverPort,result);
         return new CommonResult(500,"查询失败,对应id为"+id);
+    }
+    @Autowired
+    private DiscoveryClient discoveryClient; //可以获取已经注册到Eureka中到微服务的信息,注意需要EnableEurekaClient注解
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        //获取已经注册到Eureka中的微服务的服务名列表
+        List<String> services = discoveryClient.getServices();
+        services.forEach(p->{System.err.println("注册到Eureka的微服务名称："+p);});
+        //根据服务名称查找Eureka上的所有实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(p->{
+            System.err.println("服务id: "+p.getServiceId()+"\n"+"实例id："+p.getInstanceId()
+                    +"主机名称："+p.getHost()+"\n"+"服务端口："+p.getPort()+"\n"+"服务的URI："+p.getUri());
+        });
+        return this.discoveryClient;
     }
 
 
