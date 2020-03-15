215617e80561782ce25873972f6a291dd9a3d6a2
rumwei <isguwei@163.com>
2020-03-15 20:07:56 +0800

    注释fix

--- cloud-consumer-feign-order80/src/main/java/com/rumwei/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/rumwei/springcloud/service/PaymentFeignService.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /**
- * 该接口中的方法定义与服务提供方的服务接口定义PaymentService接口保持一致
- * 具体可以查看8001，8002两个项目
+ * 该接口中的方法定义与服务提供方的Controller保持一致
+ * 具体可以查看8001，8002两个项目的PaymentController类
 * */
 @Component
 @FeignClient(value = "CLOUD-PAYMENT-SERVICE")
