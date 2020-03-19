450c1865f5e1a329d8c20a93439c72be0270a80b
rumwei <isguwei@163.com>
2020-03-19 13:52:10 +0800

    集成springcloud bus实现配置动态刷新

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ a demo project of spring cloud
 服务提供方集群(cloud-provider-payment8001,cloud-provider-payment8002)
 Eureka注册中心集群(cloud-eureka-server7001,cloud-eureka-server7002)
 说明：未使用rpc框架，通过Eureka的服务名(有多台主机ip)，结合ribbon的@LoadBalanced注解获取到主机名，然后通过RestTemplate来完成consumer调用provider服务的目的
+## v5.0_springcloud_bus
+配置中心服务器(cloud-config-center3344)
+配置中心客户端(cloud-config-client3355,cloud-config-client3366)
+Eureka注册中心集群(cloud-eureka-server7001,cloud-eureka-server7002)
+集成SpringCloud Bus实现当配置更新时，通过发送刷新消息给3344，3344能够一次性全部通知所有配置中心客户端(3355和3366)
+也能够只通知其中的部分客户端(如只通知3355刷新，忽略3366的刷新)
--- cloud-config-client3366/src/main/java/com/rumwei/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3366/src/main/java/com/rumwei/springcloud/controller/ConfigClientController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope //实现配置的动态刷新，否则更新了github，3344可以同步拿到最新值，但是3355还是使用老配置值
-//具体步骤：在github上更新了配置之后，因为已经引入了cloud bus，此时只需要用户手动通过http请求刷新一下配置中心
+//具体步骤：1.在github上更新了配置之后，因为已经引入了cloud bus，此时只需要用户手动通过http请求刷新一下配置中心
 //服务端3344：curl -X POST "http://localhost:3344/actuator/bus-refresh"即可完成3355和3366的刷新
-//2.在github上更新了配置之后，如果只想通知本实例(3355)，而让3366还是使用老配置，则用户手动通过http请求刷新配置中心服务
-//端3344：curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355"即可只刷新3355。其中config-client即
+//2.在github上更新了配置之后，如果只想通知本实例(3366)，而让3355还是使用老配置，则用户手动通过http请求刷新配置中心服务
+//端3344：curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3366"即可只刷新3366。其中config-client即
 //为bootstrap.yml中的spring.application.name配置的应用名
 public class ConfigClientController {
     @Value("${env}")
